d18d82c345ce5594a2f0bb733ac3219aac6b47c1
im100ruv <karnasaurabh123@gmail.com>
2018-02-16 20:02:20 +0530

    design changes

---
+++ SmackChat/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  SmackChat
+//
+//  Created by Im100ruv on 16/02/18.
+//  Copyright © 2018 Im100ruv. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
