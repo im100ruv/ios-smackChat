a764e8d52101d9ddf79fcc77be6ec2763c0ba175
im100ruv <karnasaurabh123@gmail.com>
2018-02-16 23:24:50 +0530

    login design

---
+++ SmackChat/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  SmackChat
+//
+//  Created by Im100ruv on 16/02/18.
+//  Copyright © 2018 Im100ruv. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ SmackChat/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SmackChat
+//
+//  Created by Im100ruv on 16/02/18.
+//  Copyright © 2018 Im100ruv. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
+
