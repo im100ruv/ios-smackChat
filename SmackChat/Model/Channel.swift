1efc2af56ae2048c557f036ac8904c65e6a237e3
im100ruv <karnasaurabh123@gmail.com>
2018-02-19 21:49:30 +0530

    finding channels

---
+++ SmackChat/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  SmackChat
+//
+//  Created by Im100ruv on 19/02/18.
+//  Copyright © 2018 Im100ruv. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle : String!
+    public private(set) var channelDescription : String!
+    public private(set) var id : String!
+}
