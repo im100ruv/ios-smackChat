6bab5f7a9ebd362abe82c6787fe5daa81286c0b5
im100ruv <karnasaurabh123@gmail.com>
2018-02-20 15:02:14 +0530

    channel adding module works

---
+++ SmackChat/Services/SocketService.swift
@@ -0,0 +1,47 @@
+//
+//  SocketService.swift
+//  SmackChat
+//
+//  Created by Im100ruv on 20/02/18.
+//  Copyright © 2018 Im100ruv. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+    var manager = SocketManager(socketURL: URL(string: BASE_URL)!)
+    lazy var socket = manager.defaultSocket                 // var or let is not working here but lazy var works...  It may cause issues
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDescription = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDescription, id: channelId)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+    
+}
