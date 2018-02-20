2d667b37893a6a5ddf3affc78e384a04c4ccb803
im100ruv <karnasaurabh123@gmail.com>
2018-02-20 17:26:21 +0530

    getting messages for channels.... and issues solved

---
+++ SmackChat/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  SmackChat
+//
+//  Created by Im100ruv on 20/02/18.
+//  Copyright © 2018 Im100ruv. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}
